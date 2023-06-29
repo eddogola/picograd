6aafa8a63761129bb44849688cd26abced3d6771
Eddy Ogola <eddy.ogola1@gmail.com>
2023-06-29 02:12:11 -0700

    add parents with grad

--- main.py
+++ main.py
@@ -1,37 +1,62 @@
-class Val():  # scalar
-    def __init__(self, val: float):
+import math
+
+
+class Val:  # scalar
+    def __init__(self, val: float, parents=None):
+        parents = [] if not parents else parents
         self.value = val
+        self.parents = parents
+        self.grad = 0.0
 
     def __str__(self) -> str:
-        return f"{self.value}"
+        return f"Var(v={self.value}, grad={self.grad})"
 
-    def __add__(self, other):
-        return Val(self.value + other.value)
+    def __add__(self: "Val", other: "Val") -> "Val":
+        # parent: (parent, gradient)
+        return Val(self.value + other.value, parents=[(self, 1.0),
+                                                      (other, 1.0)])
 
-    def __radd__(self, other):
-        return Val(self.value + other.value)
+    def __mul__(self: "Val", other: "Val") -> "Val":
+        return Val(self.value * other.value, parents=[(self, other.value),
+                                                      (other, self.value)])
 
-    def __mul__(self, other):
-        return Val(self.value * other.value)
+    def __pow__(self: "Val", power: float):
+        return Val(self.value**power, parents=[(self, power *
+                                                self.value**(power-1))])
 
-    def __rmul__(self, other):
-        return Val(self.value * other.value)
+    def __neg__(self: "Val") -> "Val":
+        return Val(-1*self.value)
 
-    def __pow__(self, other):
-        return Val(self.value ** other.value)
+    def __sub__(self: "Val", other: "Val") -> "Val":
+        return self + (-other)
 
-    def __rpow__(self, other):
-        return Val(other.value ** self.value)
+    def __truediv__(self: "Val", other: "Val") -> "Val":
+        return self * other**(-1)
 
-    def diff(self):
-        pass
+    def tanh(self: "Val") -> "Val":
+        return Val(math.tanh(self.value),
+                   parents=[(self, 1 - math.tanh(self.value)**2)])
+
+    def backward(self):
+        self.backprop(1.0)
+
+    def backprop(self, gradient: float):
+        self.grad += gradient
+        if not self.parents:
+            return
+        for parent, grad in self.parents:
+            parent.backprop(grad * gradient)  # chain rule
 
 
 if __name__ == "__main__":
-    x = Val(3.0)
-    y = Val(5.0)
+    a = Val(3.0)
+    b = Val(5.0)
+    x = Val(1.0)
 
-    exp1 = Val(3)*Val(1)**Val(2) + Val(5)*Val(1) + Val(9)  # 3x^2 + 5x + 9; let x = 1
+    exp1 = (
+        a * x ** 2 + b * x + Val(9)
+    )  # 3x^2 + 5x + 9; let x = 1
+    exp1.backward()
 
-    print(y**Val(2))
-    print(exp1)
+    for v in [x, exp1]:
+        print(v)
